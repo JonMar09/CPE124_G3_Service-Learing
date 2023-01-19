ddad76480e01dbc56299173d4971cfad37f30daa
JonMar09 <111258860+JonMar09@users.noreply.github.com>
2023-01-20 00:11:59 +0800

    Update SLTestG3_FinalCode.ino

--- SLTestG3_FinalCode.ino
+++ SLTestG3_FinalCode.ino
@@ -1,11 +1,11 @@
-#include <DHT.h>
-#include <ESP8266WiFi.h>
-#define DHTPIN D1  
-#define DHTTYPE DHT11
+#include <DHT.h> //library for DHT11
+#include <ESP8266WiFi.h> //Library for ESP8266
+#define DHTPIN D1  //set dht pin to D1
+#define DHTTYPE DHT11 // set type of dht
 
 //Define the name and password of the WiFi network.
-const char* ssid = "virus"; 
-const char* password = "AntoMar304"; 
+const char* ssid = "xxxxx"; 
+const char* password = "xxxxx"; 
 
 WiFiServer server(80);
 DHT dht(DHTPIN, DHTTYPE);
@@ -34,20 +34,20 @@ void loop() {
     return;
   }
   // Read the temperature and humidity from the sensor
-  float h = dht.readHumidity();
   float t = dht.readTemperature();
+  float h = dht.readHumidity();
   //HTML Output of the web server
   String html = "<html><body>";
   html += "<h1>Temperature: " + String(t) + "C</h1>"; //Displays "Temperature:" in web server
   html += "<h1>Humidity: " + String(h) + "%</h1>"; //Displays "Humidity:" in web server
   html += "</body></html>";
-
-  client.println("HTTP/1.1 200 OK");
+  
+  client.println("HTTP/1.1 200 OK"); // Means server connected successfully
   client.println("Content-Type: text/html");
   client.println("Connection: close");
   client.println();
   client.println(html);
 
-  delay(1000);
+  delay(1000); // delay output for 1ms
   client.stop();
 }
